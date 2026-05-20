dd261400842266da8ba8cd1a13bc77f6b88b6a3a
Mathieu Ledru <matyo91@gmail.com>
2026-05-20 22:27:03 +0200

    :safety_vest: Unifomise CI

--- tools/phpstan/castor.php
+++ tools/phpstan/castor.php
@@ -13,7 +13,7 @@ use function Castor\run;
 function phpstan(): int
 {
     return run(
-        [__DIR__ . '/vendor/bin/phpstan', '--configuration=' . __DIR__ . '/phpstan.neon', '--memory-limit=1024M'],
+        [__DIR__ . '/vendor/bin/phpstan', 'analyse', '--configuration=' . __DIR__ . '/phpstan.neon', '--memory-limit=1024M'],
         context()->withAllowFailure(),
     )->getExitCode();
 }
